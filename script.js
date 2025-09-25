31a1a5b3986149577a84ca94d6c17ac85182c236
Mona Salem <96212168+fakhrisalem@users.noreply.github.com>
2025-09-24 19:45:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const accessKey = "RZEIOVfPhS7vMLkFdd2TSKGFBS4o9_FmcV1Nje3FSjw";
-    const formel=document.querySelector("form");
-    const searchInputEl=document.getElementById("search-input");
-    const searchResultsEl=document.querySelector(".search-results");
-    const showMoreButtonEl=document.getElementById("show-more-button");
-    let inputData="";
-    let page=1;
+    
+const formel=document.querySelector("form");
+    
+const searchInputEl=document.getElementById("search-input");
+   
+const searchResultsEl=document.querySelector(".search-results");
+   
+const showMoreButtonEl=document.getElementById("show-more-button");
+   
+let inputData="";
+    
+let page=1;
     async function searchImages(){
       inputData=searchInputEl.value;
       const url=`https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
